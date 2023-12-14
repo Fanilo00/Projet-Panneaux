8039e327c6d2a6cb17e567a5694cbb080b7218d1
TokyHenintsoa18 <tokyramanalina@gmail.com>
2023-12-14 13:42:01 +0300

    data
    bdd.sql

---
+++ Panneaux/data/bdd.sql
@@ -0,0 +1,70 @@
+--puissance batterie:24000W
+create table batterie
+(
+    id_batterie serial primary key,
+    puissance_batterie int
+);
+
+-- puissance panneau:35000W
+create table panneau
+(
+    id_panneau serial primary key,
+    puissance_panneau int,
+    id_batterie integer references batterie(id_batterie)
+);
+
+
+create table classe
+(
+    id_classe serial primary key,
+    nom_classe varchar(40)
+);
+
+
+
+create table section
+(
+    id_section serial primary key ,
+    nom_section varchar(40)
+);
+
+
+create table section_classe_panneau
+(
+    id_section integer references section(id_section),
+    id_panneau integer references panneau(id_panneau),
+    id_classe integer references classe(id_classe)
+);
+
+
+
+
+create table journale_etudiant
+(
+    id_journal_etudiant serial primary key,
+    date_heure timestamp,
+    nbr_etudiant int,
+    statut int,
+    id_classe integer references classe(id_classe)
+);
+
+
+create table journale_lumiere
+(
+    id_journale_lumiere serial primary key,
+    date_heure_lumiere timestamp,
+    niveau decimal(2,1)
+);
+
+
+
+
+create table journale_coupure
+(
+    id_journale_coupure serial primary key,
+    date_heure_coupure_vrai timestamp,
+    date_heure_coupure_prevision timestamp,
+    id_section integer references section(id_section)
+);
+
+
---
+++ Panneaux/data/view.sql
@@ -0,0 +1,43 @@
+--puissance de panneau par section
+create or replace view puissance_panneau_par_section as 
+select id_section,panneau.id_panneau,puissance_panneau,id_classe
+ from section_classe_panneau 
+join panneau on panneau.id_panneau=section_classe_panneau.id_panneau;
+
+
+
+-- puissance des panneau*nombre de panneau PAR SECTION
+create or replace view puissance_total_panneau_par_section as 
+SELECT id_section, puissance_panneau * COUNT(puissance_panneau) AS total_puissance
+FROM puissance_panneau_par_section
+GROUP BY id_section, puissance_panneau;
+
+
+
+
+
+--puissance de batterie par section 
+create or replace view puissance_batterie_par_section as
+select id_section,puissance_batterie from section_classe_panneau 
+join panneau on section_classe_panneau.id_panneau=panneau.id_panneau 
+join batterie on panneau.id_batterie=batterie.id_batterie;
+
+create or replace view puissance_total_batterie_par_Section as 
+select id_section,puissance_batterie* COUNT(puissance_batterie) as total_puissance_batterie
+from puissance_batterie_par_section
+group by id_section,puissance_batterie;
+
+
+create or replace view view_etudiant as 
+select journale_etudiant.id_journal_etudiant,date_heure,statut,nbr_etudiant,journale_etudiant.id_classe,id_section 
+from journale_etudiant join section_classe_panneau on journale_etudiant.id_classe = section_classe_panneau.id_classe;
+
+-- moyenne nbr_etudiant
+    create or replace view moy_etu as
+    SELECT DISTINCT
+    date_heure, statut, id_section, ROUND(AVG(nbr_etudiant) OVER (PARTITION BY date_heure, statut, id_section)) 
+    AS moyenne_etudiant FROM journale_etudiant JOIN section_classe_panneau 
+    ON journale_etudiant.id_classe = section_classe_panneau.id_classe;
+
+
+    
