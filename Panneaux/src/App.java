30eab38829083ec4989853813455597c8bdc21a3
Fanilo00 <faniloandriambolanoro@gmail.com>
2023-12-15 08:33:57 +0300

    Update App.java

--- Panneaux/src/App.java
+++ Panneaux/src/App.java
@@ -9,6 +9,7 @@ import java.sql.Timestamp;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        
         String url = "jdbc:mysql://localhost:3306/solaire";
         String user = "postgres";
         String password = "root";
@@ -17,17 +18,23 @@ public class App {
         {
             // Journal_lumiere j = new Journal_lumiere();
             // Journal_lumiere[] lumiere = j.niveau_lumiere();
+
             
             // Connection connection = DriverManager.getConnection(url, user, password) {
             // Journal_coupure journal = new Journal_coupure();
             // Timestamp vraieCoupure = Timestamp.valueOf("2023-12-14 16:00:00");
 
+
             // Timestamp prevision = journal.estimationCoupure(vraieCoupure);
             // System.out.println("La prévision de coupure est : " + prevision);
             
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
             } catch (Exception e) {
                 // TODO: handle exception
                 e.printStackTrace();
             }
         }
+    }
 }
